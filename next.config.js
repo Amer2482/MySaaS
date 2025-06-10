770f02c3ec6d00ceec4730cd914b00fd3e34b85f
Amer Al.Adas <amer.adas@icloud.com>
2025-06-10 19:39:41 +0400

    Deploy SaaS Application to Netlify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     domains: [
       // NextJS <Image> component needs to whitelist domains for src={}
       "lh3.googleusercontent.com",
